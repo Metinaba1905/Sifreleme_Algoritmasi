f6e8baedf4c1a9ce7f44d0e52ed618f932c25f43
Metin Aba <abametin722@gmail.com>
2022-09-24 18:47:16 +0300

    öğrenci bilgi sistemi oluşturuldu

---
+++ stajokulu-spring-project-main/stajokulu-spring-project-main/emlak.java
@@ -0,0 +1,46 @@
+import  java.util.S
+
+public class emlak {
+    private String evTipi;
+    private String il;
+    private int odaSayisi;
+    private double fiyat;
+
+
+    public String getEvTipi() {
+        return evTipi;
+    }
+
+    public void setEvTipi(String evTipi) {
+        this.evTipi = evTipi;
+    }
+    public String getIl(){
+        return evTipi;
+    }
+
+    public void setIl(String il){
+        this.il=il;
+    }
+
+    public int getOdaSayisi(){
+        return  odaSayisi;
+    }
+
+    public void setOdaSayisi (int odaSayisi){
+        this.odaSayisi=odaSayisi;
+    }
+
+    public double getFiyat(){
+        return fiyat;
+    }
+
+    public void setFiyat(double fiyat){
+        this.fiyat=fiyat;
+    }
+
+
+    emlak (String evTipi,String il,int ,double fiyat){
+
+        Scanner
+    }
+}
